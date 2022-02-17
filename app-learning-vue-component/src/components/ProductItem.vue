108bf47c92e91b64146e8c2bfde5f375b89c5ae4
long14xtnd <longdoan14xtnd@gmail.com>
2022-02-17 11:08:35 +0700

    using v-for with prop

--- app-learning-vue-component/src/components/ProductItem.vue
+++ app-learning-vue-component/src/components/ProductItem.vue
@@ -1,22 +1,21 @@
 <template>
   <div class="card text-left">
-                <img class="card-img-top" :src="urlImage" alt="">
-                <div class="card-body">
-                  <h4 class="card-title">{{ nameProduct }}</h4>
-                
-                </div>
-              </div>
+    <img class="card-img-top" :src="proItem.urlImage" alt="" />
+    <div class="card-body">
+      <h4 class="card-title">{{ proItem.nameProduct }}</h4>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-     props: {
+  props: {
     urlImage: String,
     nameProduct: String,
-  }
-}
+    proItem: Object,
+  },
+};
 </script>
 
 <style>
-
 </style>
